7e4c8f1bb083fb9e5e6dd3500da922d0d2844536
Alexandra Mirtcheva <alex@solanalabs.io>
2018-09-12 22:02:37 -0700

    multiple choice changes

--- src/components/questions/MultipleChoice.js
+++ src/components/questions/MultipleChoice.js
@@ -7,7 +7,21 @@ class MultipleChoice extends Component {
   render() {
     return (
       <div className="multiple-choice">
-        <Title>How well did I display courage?</Title>
+        <Title>Is this person up to date with the latest accounting regulations?</Title>
+        <ul className="multi-option-wrapper">
+          <li>
+            <span className="multi-low">No</span>
+            <p>You should work on trying to stay more up to date with regulations.</p>
+          </li>
+          <li>
+            <span className="multi-medium">Somewhat</span>
+            <p>You are reasonably up to date with new regulations.</p>
+          </li>
+          <li>
+            <span className="multi-high">Yes</span>
+            <p>Yes, you are the one I look up to when I need information about new regulations.</p>
+          </li>
+        </ul>
       </div>
     )
   }

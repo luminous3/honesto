50aaa1a14a44532c1c3c42cdb57496d7f7a33bf4
Alexandra Mirtcheva <alex@solanalabs.io>
2018-09-12 20:15:17 -0700

    refactor main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,59 +1,13 @@
 import React, { Component } from 'react'
 
-import person1 from './person1.png'
-import person2 from './person2.png'
-import person3 from './person3.png'
-import person4 from './person4.png'
-import person5 from './person5.png'
-
+import Home from '../share-feedback/home/Home'
 import './Main.css'
 
 class Main extends Component {
   render() {
     return (
       <main>
-        <div className="content">
-          <div className="main-header">
-            <h2 className="main-title">Share Feedback</h2>
-          </div>
-          <ul className="people-list">
-            <li>
-              <img alt="person1.png" src={person1} />
-              <span className="person-name">Chris Johnson</span>
-              <div className="btn-wrapper">
-                <button className="fill-out-btn">Fill Out</button>
-              </div>
-            </li>
-            <li>
-              <img alt="person2.png" src={person2} />
-              <span className="person-name">Nico Perez</span>
-              <div className="btn-wrapper">
-                <button className="view-btn">View Submission</button>
-              </div>
-            </li>
-            <li>
-              <img alt="person3.png" src={person3} />
-              <span className="person-name">Nathaniel Moon</span>
-              <div className="btn-wrapper">
-                <button className="view-btn">View Submission</button>
-              </div>
-            </li>
-            <li>
-              <img alt="person4.png" src={person4} />
-              <span className="person-name">Denis Denison</span>
-              <div className="btn-wrapper">
-                <button className="view-btn">View Submission</button>
-              </div>
-            </li>
-            <li>
-              <img alt="person5.png" src={person5} />
-              <span className="person-name">Paul Carter</span>
-              <div className="btn-wrapper">
-                <button className="view-btn">View Submission</button>
-              </div>
-            </li>
-          </ul>
-        </div>
+        <Home />
       </main>
     )
   }
---
+++ src/components/share-feedback/home/Home.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+import person1 from './person1.png'
+import person2 from './person2.png'
+import person3 from './person3.png'
+import person4 from './person4.png'
+import person5 from './person5.png'
+
+import './Home.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="content">
+        <div className="main-header">
+          <h2 className="main-title">Share Feedback</h2>
+        </div>
+        <ul className="people-list">
+          <li>
+            <img alt="person1.png" src={person1} />
+            <span className="person-name">Chris Johnson</span>
+            <div className="btn-wrapper">
+              <button className="fill-out-btn">Fill Out</button>
+            </div>
+          </li>
+          <li>
+            <img alt="person2.png" src={person2} />
+            <span className="person-name">Nico Perez</span>
+            <div className="btn-wrapper">
+              <button className="view-btn">View Submission</button>
+            </div>
+          </li>
+          <li>
+            <img alt="person3.png" src={person3} />
+            <span className="person-name">Nathaniel Moon</span>
+            <div className="btn-wrapper">
+              <button className="view-btn">View Submission</button>
+            </div>
+          </li>
+          <li>
+            <img alt="person4.png" src={person4} />
+            <span className="person-name">Denis Denison</span>
+            <div className="btn-wrapper">
+              <button className="view-btn">View Submission</button>
+            </div>
+          </li>
+          <li>
+            <img alt="person5.png" src={person5} />
+            <span className="person-name">Paul Carter</span>
+            <div className="btn-wrapper">
+              <button className="view-btn">View Submission</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Home

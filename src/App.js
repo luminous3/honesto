fdf42bf16fc6ce3b26e35e6f2a06168cf23251ac
Alexandra Mirtcheva <alex@solanalabs.io>
2018-09-12 18:18:47 -0700

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import logoImage from './img/Theorem.png'
+import logoIcon from './img/halmos.png'
+import avatar from './img/avatar.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="container">
+        <header>
+          <h1 className="title">Honesto</h1>
+          <ul className="header-menu">
+            <li>Share Feedback</li>
+            <li>My Feedback</li>
+            <li>Team Feedback</li>
+            <li>Teams</li>
+            <li className="cycle-wrapper">
+              <span className="info">Next feedback cycle:</span>
+              <span className="date">
+                Sept 2018 - <span className="days">4 days</span>
+              </span>
+            </li>
+          </ul>
+          <div className="login-wrapper">
+            <img alt="avatar.png" src={avatar} />
+            <div className="login-info">
+              <span className="name">Jane Smith</span>
+              <span className="logout">LOGOUT</span>
+            </div>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section>
+          <h1 className="share-title">Share Feedback</h1>
+        </section>
+        <footer>
+          <span>
+            <img alt="theorem.png" src={logoImage} /> <img alt="halmos.png" src={logoIcon} />
+          </span>
+          <div className="copyright">Copyright &copy;2018 Theorem, LLC. All rights reserved. </div>
+        </footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

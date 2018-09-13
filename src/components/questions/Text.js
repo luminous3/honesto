d3c81df3e8f03dc50e6bb31b3d7b8ba9bd7dd172
Alexandra Mirtcheva <alex@solanalabs.io>
2018-09-13 15:52:10 -0700

    dynamic loading and saving of text questions

--- src/components/questions/Text.js
+++ src/components/questions/Text.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
+import { updateFeedback } from '../../state/actions'
 import Title from '../common/Title'
 import QuestionContainer from './QuestionContainer'
 import './Text.css'
 
 class Text extends Component {
   render() {
+    const { question } = this.props
     return (
       <div className="text">
-        <Title>
-          Have there been any situations where this person could have managed their emotions better? What happened?
-        </Title>
+        <Title>{question.value}</Title>
         <QuestionContainer>
-          <textarea placeholder="Say something" />
+          <textarea onChange={e => this.props.update(e.target.value)} placeholder="Say something" />
         </QuestionContainer>
       </div>
     )
   }
 }
 
-export default Text
+const mapDispatchToProps = (dispatch, props) => {
+  const { user, question } = props
+  return {
+    update: feedback =>
+      dispatch(
+        updateFeedback(user.id, {
+          q_id: question.id,
+          type: 'text',
+          data: feedback,
+        }),
+      ),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Text)
